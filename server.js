b1893660834303f7bc7985f4e435ca6851fd8108
serge <sergekv79@gmail.com>
2018-07-01 17:14:16 +0300

    Add PORT

--- server.js
+++ server.js
@@ -11,5 +11,6 @@ const server = http.createServer(function(request, response) {
 	}
 }) ;
 
-server.listen(3000);
+console.log('port = ', process.env.PORT);
+server.listen(process.env.PORT || 3000);
 console.log('Server started');
